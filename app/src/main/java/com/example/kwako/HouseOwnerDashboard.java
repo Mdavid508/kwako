f0202fbc86055b058d6b4af4e03475dc2180f648
Mugo David <mugombataru@outlook.com>
2023-02-28 16:35:33 +0300

    adding of backend

--- app/src/main/java/com/example/kwako/HouseOwnerDashboard.java
+++ app/src/main/java/com/example/kwako/HouseOwnerDashboard.java
@@ -2,7 +2,9 @@ package com.example.kwako;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HouseOwnerDashboard extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class HouseOwnerDashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house_owner_dashboard);
+
+        Button addProperty= findViewById(R.id.addProperty);
+
+        addProperty.setOnClickListener(view ->{
+            Intent intent = new Intent(HouseOwnerDashboard.this, PostProperty.class);
+            startActivity(intent);
+        });
+
+        //logout from the activity
+        Button houseOwnerLogout = findViewById(R.id.logout);
+
+        houseOwnerLogout.setOnClickListener(view -> {
+            Intent intent = new Intent(this, ActivityLogin.class);
+            startActivity(intent);
+        });
+
     }
 }
