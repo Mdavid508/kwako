26167615872e61b3ab70603d9150dd267c4baceb
Mugo David <mugombataru@outlook.com>
2023-03-17 16:46:45 +0300

    addition of recycler view in upload image layout

---
+++ app/src/main/java/com/example/kwako/image_upload_view.java
@@ -0,0 +1,14 @@
+package com.example.kwako;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class image_upload_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_upload_view);
+    }
+}
