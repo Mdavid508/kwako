4ada51376de04c3f4dd7a515aafcc77cfcaf91e4
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-03-22 04:49:30 +0300

    Define viewHolder inside the same adapter class

--- app/src/main/java/com/example/kwako/adapters/ImageUploadAdapter.java
+++ app/src/main/java/com/example/kwako/adapters/ImageUploadAdapter.java
@@ -2,18 +2,19 @@ package com.example.kwako.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.kwako.ImageUploadViewHolder;
-import com.example.kwako.Item;
+import com.example.kwako.models.Item;
 import com.example.kwako.R;
 
 import java.util.List;
 
-public class ImageUploadAdapter extends RecyclerView.Adapter<ImageUploadViewHolder> {
+public class ImageUploadAdapter extends RecyclerView.Adapter<ImageUploadAdapter.ImageUploadViewHolder> {
 
     Context context;
     List<Item> items;
@@ -26,7 +27,7 @@ public class ImageUploadAdapter extends RecyclerView.Adapter<ImageUploadViewHold
     @NonNull
     @Override
     public ImageUploadViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ImageUploadViewHolder(LayoutInflater.from(context).inflate(R.layout.activity_image_upload_item,parent,false));
+        return new ImageUploadViewHolder(LayoutInflater.from(context).inflate(R.layout.image_upload_item,parent,false));
     }
 
     @Override
@@ -38,5 +39,19 @@ public class ImageUploadAdapter extends RecyclerView.Adapter<ImageUploadViewHold
     @Override
     public int getItemCount() {
         return items.size();
+    }
+
+    /**
+     * You can define your viewHolder class here
+     */
+    public class ImageUploadViewHolder extends RecyclerView.ViewHolder {
+        TextView uploadNameView;
+        ImageView ivDelete;
+
+        public ImageUploadViewHolder(@NonNull View itemView) {
+            super(itemView);
+            uploadNameView = itemView.findViewById(R.id.tvUploadName);
+            ivDelete = itemView.findViewById(R.id.ivDelete);
+        }
     }
 }
