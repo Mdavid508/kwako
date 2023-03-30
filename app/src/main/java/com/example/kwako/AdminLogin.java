7651252fc3062852487c2df4efac7282a83bf762
asenawritescode <samasena2001@gmail.com>
2023-03-30 18:03:40 +0300

    Mpesa functionality.

--- app/src/main/java/com/example/kwako/AdminLogin.java
+++ app/src/main/java/com/example/kwako/AdminLogin.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,11 +16,14 @@ public class AdminLogin extends AppCompatActivity {
     EditText edtEmail;
     EditText edtPassword;
     Button btnLogin;
+
     String email;
     String password;
     ProgressDialog loader;
     FirebaseAuth mAuth;
 
+    // To be removed
+    Button directLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,15 @@ public class AdminLogin extends AppCompatActivity {
         btnLogin=findViewById(R.id.button_login);
         loader=new ProgressDialog(this);
         mAuth= FirebaseAuth.getInstance();
+
+        //To be removed
+        directLog=findViewById(R.id.button_remove);
+        // Listener for the direct link button
+
+        directLog.setOnClickListener(view -> {
+            Intent houseBook = new Intent(AdminLogin.this, ActivityHouseBooking.class);
+            startActivity(houseBook);
+        });
 
         // listen for btnRegister click
         btnLogin.setOnClickListener(v -> {
