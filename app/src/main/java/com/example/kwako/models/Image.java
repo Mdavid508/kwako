286fd23a23cb19ca0c3f9c9c6791aa1b25d4de8f
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-03-31 04:59:56 +0300

    Upload image with an image URI

--- app/src/main/java/com/example/kwako/models/Image.java
+++ app/src/main/java/com/example/kwako/models/Image.java
@@ -1,14 +1,22 @@
 package com.example.kwako.models;
 
+import android.net.Uri;
+
 public class Image {
     String imageUrl, imageName;
+    Uri imageUri;
 
-    public Image(String imageUrl){
-        this.imageUrl = imageUrl;
-    }
-    public Image(String imageUrl, String imageName) {
-        this.imageUrl = imageUrl;
+    public Image(String imageName){
         this.imageName = imageName;
+    }
+    public Image(String imageName, Uri imageUri) {
+        this.imageName = imageName;
+        this.imageUri = imageUri;
+    }
+    public Image(String imageName, Uri imageUri, String imageUrl) {
+        this.imageName = imageName;
+        this.imageUri = imageUri;
+        this.imageUrl = imageUrl;
     }
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
@@ -16,12 +24,16 @@ public class Image {
     public String getImageUrl() {
         return imageUrl;
     }
-
     public String getImageName() {
         return imageName;
     }
-
     public void setImageName(String imageName) {
         this.imageName = imageName;
+    }
+    public Uri getImageUri() {
+        return imageUri;
+    }
+    public void setImageUri(Uri imageUri) {
+        this.imageUri = imageUri;
     }
 }
